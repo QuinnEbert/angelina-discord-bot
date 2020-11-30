9cdc83cbf182efd47dd89f25f8e2d353e393f719
Quinn Ebert <quinnebert@me.com>
2020-11-30 18:49:46 -0500

    Attempt to fix issues by using absolute paths in the bootstrap script

--- Angelina.sh
+++ Angelina.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/quinn/Sources/DiscordBot
-while true ; do ./Angelina.py &> Angelina.out ; done
+while true ; do /home/quinn/Sources/DiscordBot/Angelina.py &> /home/quinn/Sources/DiscordBot/Angelina.out ; done
